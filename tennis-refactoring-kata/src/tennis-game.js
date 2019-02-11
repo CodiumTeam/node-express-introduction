0c284ab74f3af0f0637e675b2cd558fc3cd83dc7
Luis Rovirosa <luisrovirosa@gmail.com>
2019-02-11 11:54:16 +0100

    tennis game: getScoreNearToEndGame flip if

--- tennis-refactoring-kata/src/tennis-game.js
+++ tennis-refactoring-kata/src/tennis-game.js
@@ -34,10 +34,10 @@ TennisGame.prototype.getScoreWithSameScore = function() {
 TennisGame.prototype.getScoreNearToEndGame = function() {
     let score;
     var minusResult = Math.abs(this.m_score1 - this.m_score2);
-    if (minusResult === 1) {
-        score = 'Advantage ' + this.winningPlayer();
-    } else if (this.gameHasEnded()) {
+    if (this.gameHasEnded()) {
         score = 'Win for ' + this.winningPlayer();
+    } else if (minusResult === 1) {
+        score = 'Advantage ' + this.winningPlayer();
     }
     return score;
 };
