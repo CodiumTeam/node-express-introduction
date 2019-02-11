ba95ceda5cc1bcde9c39efb8e265786acd45f545
Luis Rovirosa <luisrovirosa@gmail.com>
2019-02-11 11:36:11 +0100

    tennis game: fix problem with player1 Name

--- tennis-refactoring-kata/src/tennis-game.js
+++ tennis-refactoring-kata/src/tennis-game.js
@@ -6,7 +6,7 @@ var TennisGame = function(player1Name, player2Name) {
 };
 
 TennisGame.prototype.wonPoint = function(playerName) {
-    if (playerName === 'player1')
+    if (playerName === this.player1Name)
         this.m_score1 += 1;
     else
         this.m_score2 += 1;
