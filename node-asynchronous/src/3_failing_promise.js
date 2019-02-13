d4a97cfdc809057763e58347d58e0d2cdd7edc64
Luis Rovirosa <luisrovirosa@gmail.com>
2019-02-13 06:27:18 +0100

    asynchronous minikata: solution 3

---
+++ node-asynchronous/src/3_failing_promise.js
@@ -0,0 +1,13 @@
+// First solution
+let promise = new Promise((resolve, reject) => {
+    setTimeout(reject, 1000);
+});
+
+promise.catch(() => console.log('Hello world'));
+
+// Second solution
+let secondPromise = new Promise((resolve, reject) => {
+    setTimeout(() => reject('Hello world'), 1000);
+});
+
+secondPromise.catch((message) => console.log(message));
