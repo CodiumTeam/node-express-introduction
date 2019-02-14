5e06092d34a129d9dc517a3ef56b270092f6b15e
Luis Rovirosa <luisrovirosa@gmail.com>
2019-02-14 07:16:33 +0100

    express introduction: solution of mini kata 1

---
+++ express-introduction/src/1.js
@@ -0,0 +1,13 @@
+// Access to http://localhost:3000/
+
+const express = require('express');
+const app = express();
+const port = 3000;
+
+app.get('/', (req, res) => {
+    res.send('Hello World!');
+});
+
+app.listen(port, () => {
+    console.log(`Open http://localhost:${port}`);
+});
